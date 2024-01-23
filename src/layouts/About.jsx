d0f7e004cde8b2330b8bda983dc8c234d2272ed9
rachel-aileen <137586206+rachel-aileen@users.noreply.github.com>
2024-01-22 17:35:39 -0800

    luxe - hero and about complete

--- src/layouts/About.jsx
+++ src/layouts/About.jsx
@@ -1,7 +1,33 @@
+import { Grid } from '@mui/material'
 import React from 'react'
 
 export default function About() {
     return (
-        <div className='about'>About</div>
+        <div className='about'>
+
+            <Grid container
+                direction="row"
+                alignItems="center"
+                justifyContent="space-between"
+            >
+
+                <Grid item>
+                    <div>
+                        <img className='about-img' src="https://plus.unsplash.com/premium_photo-1692574096082-d2b425cf1a5b?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                    </div>
+                </Grid>
+
+                <Grid>
+                    <h1>Hello, this is where the contenet goes.
+
+                    </h1>
+                </Grid>
+
+            </Grid>
+
+
+
+
+        </div>
     )
 }
--- src/layouts/Hero.jsx
+++ src/layouts/Hero.jsx
@@ -1,7 +1,37 @@
+import { Grid } from '@mui/material'
 import React from 'react'
 
 export default function Hero() {
     return (
-        <div className='hero'>Hero</div>
+        <div className='hero'>
+
+            <Grid container
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+            >
+
+                <Grid item>
+
+                    <div>
+                        <h1>Hello</h1>
+                    </div>
+
+                </Grid>
+
+                <Grid item>
+
+                    <div>
+                        <h1>Hello, how are you?</h1>
+                    </div>
+
+                </Grid>
+
+            </Grid>
+
+
+
+
+        </div>
     )
 }

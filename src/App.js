c0d7ef663afb9ab050fb69d1a6b4e02fab474e15
rachel-aileen <137586206+rachel-aileen@users.noreply.github.com>
2024-01-22 17:09:52 -0800

    luxe practice v1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 
 import './App.css';
+import Features from './layouts/Features';
+import About from './layouts/About';
+import Hero from './layouts/Hero';
+import Navbar from './layouts/Navbar';
 
 
 function App() {
   return (
     <div>
 
+      <Navbar />
+
+      <Hero />
+
+      <About />
+
+      <Features />
 
     </div>
   );
